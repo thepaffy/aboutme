5a9322b09b25401ef5ebfac157bfa8e3d32e5241
Christian Paffhausen <c.paffhausen@gmx.de>
2015-08-16 20:23:46 +0200

    Initial commit

---
+++ cgi/sendmail.php
@@ -0,0 +1,17 @@
+<?php
+    $name       = $_POST["name"];
+    $mail       = $_POST["mail"];
+    $message    = $_POST["message"];
+
+    $to         = 'c.paffhausen@gmx.de';
+    $subject    = 'Mail from: ' . $name . ', by thepaffy.de';
+    $message    = $_POST["message"];
+    $headers    = 'From: ' . $mail . "\r\n" .
+        'Reply-To: ' . $mail . "\r\n" .
+        "Content-Type: text/plain; charset=\"utf-8\"" . "\r\n" .
+        'X-Mailer: PHP/' . phpversion();
+
+    $success    = mail($to, $subject, $message, $headers);
+
+    echo "{ \"success\": " . ($success ? "true" : "false") . " }";
+?>
